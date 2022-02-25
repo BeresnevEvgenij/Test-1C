ed649c0583381bf50462001a2fd538a777084049
neo <beresnevevgenij24@gmail.com>
2022-02-25 19:34:16 +0300

    ....

---
+++ JS/fail.js
@@ -0,0 +1,11 @@
+// let green = document.querySelector('#green');
+//   document.querySelector('#green').onclick = function() {
+//     green.classList.add("green");
+//
+//   }
+// let red = document.querySelector('#red');
+//   document.querySelector('#red').onclick = function() {
+//     red.classList.add("red");
+//
+//   }
+////////////////////////////////////////////////////////////////////
